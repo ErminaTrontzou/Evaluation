acd5a20207cb054120b4211326ab6e2aba17a331
7ermi <7erminatrontzou7@gmail.com>
2022-11-25 17:20:07 +0200

    started form logic.

--- include/formLogic.php
+++ include/formLogic.php
@@ -2,5 +2,15 @@
     session_start();
     require('../include/config.php');
     $sessionID = $_SESSION["id"];
-    $query=mysqli_query($sql, "SELECT * FROM subject WHERE ");
-    $row = mysqli_fetch_assoc($query);
+    $studentKey = $_GET["token"];
+//    $query=mysqli_query($sql, "SELECT * FROM subject WHERE ");
+//    $row = mysqli_fetch_assoc($query);
+    $mathimaStoxoi = $_POST["3"];
+//    $mathimaUlh = $_POST["mathima-ulh"];
+//    $mathimaOrganoshYlhs = $_POST["mathima-organosh-ulh"];
+//    $mathimaYliko = $_POST["mathima-uliko"];
+//    $mathimaProhgoumenaMathimata = $_POST["mathima-prohgoumena-mathimata"];
+
+    $query=mysqli_query($sql,'SELECT * FROM evaluation_key WHERE value = "' .$studentKey.'"');
+
+    $query=mysqli_query($sql,'INSERT INTO student_answer (key_id, rating) VALUES ('.$studentKey.','.$mathimaStoxoi.')');
--- pages/form.php
+++ pages/form.php
@@ -13,18 +13,94 @@
     $teacherName = $row["last_name"]." ".$row["first_name"];
     $subjectTitle = $row["title"];
 
-//    $query=mssql_query($sql,'SELECT *
-//                                        FROM evaluation_key
-//                                        JOIN student ON student.id = ')
+    $query=mysqli_query($sql,'SELECT *
+                                        FROM question
+                                        WHERE code_name LIKE "mathima%"');
+    $row = mysqli_fetch_all($query);
+    $mathimaQuestion = array();
+    for($i=0;$i<sizeof($row);$i++){
+       $mathimaQuestion[$i]= $row[$i][2];
+    }
+    $mathimaID = array();
+    for($i=0;$i<sizeof($row);$i++){
+        $mathimaID[$i]= $row[$i][0];
+    }
+
+
+
+$query=mysqli_query($sql,'SELECT *
+                                        FROM question
+                                        WHERE code_name LIKE "askiseis%"');
+    $row = mysqli_fetch_all($query);
+    $askiseisQuestion = array();
+    for($i=0;$i<sizeof($row);$i++){
+        $askiseisQuestion[$i]= $row[$i][2];
+    }
+
+    $query=mysqli_query($sql,'SELECT *
+                                            FROM question
+                                            WHERE code_name LIKE "didaskon%"');
+    $row = mysqli_fetch_all($query);
+    $didaskonQuestion = array();
+    for($i=0;$i<sizeof($row);$i++){
+        $didaskonQuestion[$i]= $row[$i][2];
+    }
+
+    $query=mysqli_query($sql,'SELECT *
+                                            FROM question
+                                            WHERE code_name LIKE "foithths%"');
+    $row = mysqli_fetch_all($query);
+    $foiththsQuestion = array();
+    for($i=0;$i<sizeof($row);$i++){
+        $foiththsQuestion[$i]= $row[$i][2];
+    }
+
 ?>
 
 <html>
     <head>
         <title>Evaluation</title>
+        <link href="../css/formStyle.css" rel="stylesheet">
     </head>
     <body>
         <div class="title">
-            <h1>Αξιολόγηση για το μάθημα <?= $subjectTitle ?> </h1>
+            <form  method="post" action="../include/formLogic.php">
+                <h1>Αξιολόγηση για το μάθημα <?= $subjectTitle ?> του <?= $teacherName?> </h1>
+                <div align="left">
+                <h3>Ερωτήσεις για το μάθημα </h3>
+                <?php
+                for ($i = 0; $i < sizeof($mathimaQuestion); $i++ ){
+                    echo "<div> <p>".$mathimaQuestion[$i]."</div>";
+                    echo "<div>";
+                    echo "<input type='radio' value=1 name='.$mathimaID[$i].' >";
+                    echo "<input type='radio' value=2 name='.$mathimaID[$i].' >";
+                    echo "<input type='radio' value=3 name='.$mathimaID[$i].' >";
+                    echo "<input type='radio' value=4 name='.$mathimaID[$i].' >";
+                    echo "<input type='radio' value=5 name='.$mathimaID[$i].' >";
+                    echo "</p> </div>";
+                }
+                ?>
+                <h3>Ερωτήσεις για τις ασκήσεις </h3>
+                <?php
+                for ($i = 0; $i < sizeof($askiseisQuestion); $i++ ){
+                    echo "<p>".$askiseisQuestion[$i]."</p>";
+                }
+                ?>
+                <h3>Ερωτήσεις για τον καθηγητή </h3>
+                <?php
+                for ($i = 0; $i < sizeof($didaskonQuestion); $i++ ){
+                    echo "<p>".$didaskonQuestion[$i]."</p>";
+                }
+                ?>
+                <h3>Ερωτήσεις για τον Φοιτητή </h3>
+                <?php
+                for ($i = 0; $i < sizeof($foiththsQuestion); $i++ ){
+                    echo "<p>".$foiththsQuestion[$i]."</p>";
+                }
+                ?>
+                <button class="btn btn-lg searchButton" type="submit">Search</button>
+                </div>
+            </form>
         </div>
         <div>
 
