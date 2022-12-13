11c8d4e39115f3282f1fde23eb12ca2397efc2a4
7ermi <7erminatrontzou7@gmail.com>
2022-12-13 15:42:44 +0200

    Made create new question

--- pages/admin-panel.php
+++ pages/admin-panel.php
@@ -23,44 +23,73 @@
     </br></br></br>
     <img src="../images/admin.jpg">
     </br></br></br>
-    <div align="center">
-        <form action="#" method="post">
-            <select class="form-select" aria-label="Default select example" name="subjects">
-                <option selected>Διάλεξε μάθημα για αποτελέσματα</option>
-                <option value="1">Μαθηματικά</option>
-                <option value="2">Φυσική</option>
-                <option value="3">Χημεία</option>
-                <option value="4">Αστρονομία</option>
-            </select>
-            <input type="submit" name="submit" value="Υποβολή" />
-        </form>
-    </div>
-    </body>
-</html>
-
-<?php
-if(isset($_POST['submit'])){
-    $selected_val = $_POST['subjects'];
-    $query=mysqli_query($sql, 'SELECT rating
+    <div class="tabs">
+        <input class="input" name="tabs" type="radio" id="tab-1" checked="checked"/>
+        <label class="label" for="tab-1">Αποτελέσματα Αξιολογήσεων</label>
+        <div align="center" class="panel">
+            <form action="#" method="post">
+                <select class="form-select" aria-label="Default select example" name="subjectsForResult">
+                    <option selected>Διάλεξε μάθημα για αποτελέσματα</option>
+                    <option value="1">Μαθηματικά</option>
+                    <option value="2">Φυσική</option>
+                    <option value="3">Χημεία</option>
+                    <option value="4">Αστρονομία</option>
+                </select>
+                <input type="submit" name="submitResult" value="Υποβολή" />
+            </form>
+            <?php
+            if(isset($_POST['submitResult'])){
+                $selected_val = $_POST['subjectsForResult'];
+                $query=mysqli_query($sql, 'SELECT rating
                                     FROM evaluation_key
                                     JOIN student_answer ON student_answer.key_id=evaluation_key.id
                                     WHERE  subject_id ="' .$selected_val .'"');
-    $row = mysqli_fetch_all($query);
-    $mathimaRatings = 0;
-    for($i=0;$i<sizeof($row);$i++){
-        $mathimaRatings += $row[$i][0];
-    }
-    $result = sprintf('%0.3f' ,$mathimaRatings / sizeof($row));
+                $row = mysqli_fetch_all($query);
+                $mathimaRatings = 0;
+                for($i=0;$i<sizeof($row);$i++){
+                    $mathimaRatings += $row[$i][0];
+                }
+                $result = sprintf('%0.3f' ,$mathimaRatings / sizeof($row));
 
-    $query=mysqli_query($sql, 'SELECT *
+                $query=mysqli_query($sql, 'SELECT *
                                     FROM evaluation_key
                                     JOIN subject ON subject.id = evaluation_key.subject_id
                                     WHERE  subject_id ="' .$selected_val .'"');
-    $row = mysqli_fetch_assoc($query);
-    $subjectName = $row["title"];
-    echo "</br></br><div align='center' class='result'>";
-    echo "<h4>Ο μέσος όρος του μαθήματος <b>$subjectName</b> είναι:<b> $result </b> </h4>";
-    echo "</div>";
+                $row = mysqli_fetch_assoc($query);
+                $subjectName = $row["title"];
+                echo "</br></br><div align='center' class='result'>";
+                echo "<h4>Ο μέσος όρος του μαθήματος <b>$subjectName</b> είναι:<b> $result </b> </h4>";
+                echo "</div>";
 
-}
-?>
+            }
+            ?>
+        </div>
+        <input class="input" name="tabs" type="radio" id="tab-2"/>
+        <label class="label" for="tab-2">Εισαγωγή Ερώτησης</label>
+        <div class="panel">
+            <form action="#" method="post">
+                <h2>Εισαγωγή νέας Ερώτησης!</h2>
+                <label for="codeName">Code Name</label><br>
+                <input type="text" id="code_name" name="code"><br>
+                <label for="questionText">Ερώτηση</label><br>
+                <input type="text" id="question" name="question_text">
+                </br>
+                </br>
+                <input type="submit" name="submitInsert" value="Υποβολή" />
+            </form>
+            <?php
+            if(isset($_POST['submitInsert'])){
+                $codeName = $_POST['code'];
+                $question = $_POST['question_text'];
+                $query=mysqli_query($sql,"INSERT INTO question ( code_name, question) 
+                                    VALUES ( '$codeName', '$question')");
+                echo "</br></br><div align='center' class='result'>";
+                echo "<h4>Εγινε η εισαγωγη με <b>επιτυχια!</b></h4>";
+                echo "</div>";
+            }
+            ?>
+        </div>
+        </div>
+    </body>
+</html>
+
